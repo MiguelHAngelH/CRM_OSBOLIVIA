57e75392bd4271b2635f7a8aed01ac2bd87951b1
miguel angel <miguelhangelh@hotmail.com>
2016-08-30 16:18:33 -0400

    COPIA OFICIAL - CRM

---
+++ CRM_OS/CRM_OS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRM_OS.MvcApplication" Language="C#" %>
---
+++ CRM_OS/CRM_OS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRM_OS.Startup))]
+namespace CRM_OS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
